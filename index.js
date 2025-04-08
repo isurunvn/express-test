60788c5a9ea557cc49712cfd17637f1277e0e9f1
isurunvn <isurunaveen27@gmail.com>
2025-04-08 19:25:16 +0530

    fifth

--- index.js
+++ index.js
@@ -16,3 +16,26 @@ app.get("/test", (req, res) => {
 app.listen(3000, () => {
   console.log("listening on 3000");
 });
+
+
+// Dummy JavaScript code
+
+// Greet the user
+function greetUser(name) {
+  console.log("Hello, " + name + "! Welcome to our site.");
+}
+
+// Add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Generate a random number between 1 and 100
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Call the functions
+greetUser("Isuru");
+console.log("5 + 10 =", addNumbers(5, 10));
+console.log("Random number:", getRandomNumber());
