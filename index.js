659884ce84cef74032549cc678add22aef6ebe71
isurunvn <isurunaveen27@gmail.com>
2025-04-08 20:20:37 +0530

    test2

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ const express = require("express");
 
 const app = express();
 
-const unused =332;
-
-
 app.get("/", (req, res) => {
   res.send("Welcome to my app!!!!!!");
 });
@@ -18,3 +15,24 @@ app.listen(3000, () => {
 });
 
 
+// Dummy JavaScript code
+
+// Greet the user
+function greetUser(name) {
+  console.log("Hello, " + name + "! Welcome to our site.");
+}
+
+// Add two numbers
+function addNumbers(a, b) {
+  return a + b;
+}
+
+// Generate a random number between 1 and 100
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+// Call the functions
+greetUser("Isuru");
+console.log("5 + 10 =", addNumbers(5, 10));
+console.log("Random number:", getRandomNumber());
