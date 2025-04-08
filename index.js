45185b40f2a81947fdabdefe8a021f7ad89d22ba
isurunvn <isurunaveen27@gmail.com>
2025-04-08 20:18:55 +0530

    test

--- index.js
+++ index.js
@@ -18,24 +18,3 @@ app.listen(3000, () => {
 });
 
 
-// Dummy JavaScript code
-
-// Greet the user
-function greetUser(name) {
-  console.log("Hello, " + name + "! Welcome to our site.");
-}
-
-// Add two numbers
-function addNumbers(a, b) {
-  return a + b;
-}
-
-// Generate a random number between 1 and 100
-function getRandomNumber() {
-  return Math.floor(Math.random() * 100) + 1;
-}
-
-// Call the functions
-greetUser("Isuru");
-console.log("5 + 10 =", addNumbers(5, 10));
-console.log("Random number:", getRandomNumber());
