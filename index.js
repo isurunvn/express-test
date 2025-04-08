c5f60f80f0732a72a7737ed3a8a54588efa514a1
isurunvn <isurunaveen27@gmail.com>
2025-04-08 14:05:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my app!!!!!!");
+});
+
+app.get("/test", (req, res) => {
+  res.send("hey this is testing api");
+});
+
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
